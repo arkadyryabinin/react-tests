8ebe6820ec3ae603fcae2f8e18ed6b4d73956452
Arkady Ryabinin <arkadyryabinin@yandex.ru>
2022-12-06 21:27:04 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import Gall, { Profile } from './Gallery.js';
-
 function App() {
+  const list = [
+    'Invent new traffic lights',
+    'Rehearse a movie scene',
+    'Improve the spectrum technology',
+  ].map((x, i) => (<li key={i}>{x}</li>));
   return (
-    <Profile />
+    <>
+      <h1>Hedy Lamarr's Todos</h1>
+      <img 
+        src="https://i.imgur.com/yXOvdOSs.jpg" 
+        alt="Hedy Lamarr" 
+        className="photo"
+      />
+      <ul>
+        {list}
+      </ul>
+    </>
   );
 }
 
