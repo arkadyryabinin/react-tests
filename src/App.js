09abb1f22518cb57a648e428d241e4dbfd23fb36
Arkady Ryabinin <arkadyryabinin@yandex.ru>
2022-12-07 03:57:01 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,75 @@
-function App() {
-  const list = [
-    'Invent new traffic lights',
-    'Rehearse a movie scene',
-    'Improve the spectrum technology',
-  ].map((x, i) => (<li key={i}>{x}</li>));
+import { getImageUrl } from './utils.js';
+
+function Profile({ person }) {
   return (
-    <>
-      <h1>Hedy Lamarr's Todos</h1>
-      <img 
-        src="https://i.imgur.com/yXOvdOSs.jpg" 
-        alt="Hedy Lamarr" 
-        className="photo"
-      />
-      <ul>
-        {list}
-      </ul>
-    </>
-  );
+    <section className="profile">
+        <h2>{person.name}</h2>
+        <img
+          className="avatar"
+          src={getImageUrl(person.image.code)}
+          alt={person.name}
+          width={person.image.width}
+          height={person.image.height}
+        />
+        <ul>
+          <li>
+            <b>Profession: </b> 
+            {person.profession}
+          </li>
+          <li>
+            <b>Awards: {person.awards.length} </b> 
+            ({person.awards.join(', ')})
+          </li>
+          <li>
+            <b>Discovered: </b>
+            {person.discoveries.join(', ')}
+          </li>
+        </ul>
+      </section>
+  )
 }
 
-export default App;
+const persons = [
+  {
+    name: 'Maria Skłodowska-Curie',
+    image: {
+      code: 'szV5sdG',
+      width: 70,
+      height: 70
+    },
+    profession: 'physicist and chemist',
+    awards: [ 
+      'Nobel Prize in Physics',
+      'Nobel Prize in Chemistry',
+      'Davy Medal',
+      'Matteucci Medal',
+    ],
+    discoveries: ['polonium (element)'],
+  },
+
+  {
+    name: 'Katsuko Saruhashi',
+    image: {
+      code: 'YfeOqp2',
+      width: 70,
+      height: 70
+    },
+    profession: 'geochemist',
+    awards: [ 
+      'Miyake Prize for geochemistry',
+      'Tanaka Prize',
+    ],
+    discoveries: ['a method for measuring carbon dioxide in seawater'],
+  },
+  
+]
+
+export default function Gallery() {
+  const list = persons.map((x, i) => (<Profile person={x} key={i} />))
+  return (
+    <div>
+      <h1>Notable Scientists</h1>
+      {list}
+    </div>
+  );
+}
