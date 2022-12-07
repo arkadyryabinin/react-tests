5e6f6945c08e13ad58d32b0a77685f86b93c4c23
arkady ryabinin <arkadyryabinin@yandex.ru>
2022-12-07 17:17:47 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,79 +1,12 @@
-import { getImageUrl } from './utils.js';
-import { useParams } from 'react';
-console.log(useParams);
-
-function Profile({ person }) {
+export default function Toolbar() {
   return (
-    <section className="profile">
-        <h2>{person.name}</h2>
-        <img
-          className="avatar"
-          src={getImageUrl(person.image.code)}
-          alt={person.name}
-          width={person.image.width}
-          height={person.image.height}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b> 
-            {person.profession}
-          </li>
-          <li>
-            <b>Awards: {person.awards.length} </b> 
-            ({person.awards.join(', ')})
-          </li>
-          <li>
-            <b>Discovered: </b>
-            {person.discoveries.join(', ')}
-          </li>
-        </ul>
-      </section>
-  )
-}
-
-const persons = [
-  {
-    name: 'Maria Skłodowska-Curie',
-    image: {
-      code: 'szV5sdG',
-      width: 70,
-      height: 70
-    },
-    profession: 'physicist and chemist',
-    awards: [ 
-      'Nobel Prize in Physics',
-      'Nobel Prize in Chemistry',
-      'Davy Medal',
-      'Matteucci Medal',
-    ],
-    discoveries: ['polonium (element)'],
-  },
-
-  {
-    name: 'Katsuko Saruhashi',
-    image: {
-      code: 'YfeOqp2',
-      width: 80,
-      height: 70
-    },
-    profession: 'geochemist',
-    awards: [ 
-      'Miyake Prize for geochemistry',
-      'Tanaka Prize',
-    ],
-    discoveries: ['a method for measuring carbon dioxide in seawater'],
-  },
-  
-]
-
-export default function Gallery() {
-  // const { userName } = useParams();
-  // console.log(`userName: ${userName}`);
-  const list = persons.map((x, i) => (<Profile person={x} key={i} />))
-  return (
-    <div>
-      <h1>Notable Scientists</h1>
-      {list}
+    <div className="Toolbar" onInput={() => {
+      console.log('toolbar!');
+    }}>
+      <input type="text" />
+      <button onClick={() => alert('Uploading!')}>
+        Upload Image
+      </button>
     </div>
   );
 }
